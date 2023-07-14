020fc850e3994d85134bc938c1c837655e18e3f8
Hlogie01 <mllehlogonolo@gmail.com>
2023-07-14 13:48:58 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 	int c;
+
 	c = 0;
 	while
 		(c < 10) {
 			putchar(c + '0');
 			c++;
 		}
-			putchar('\n');
-			return (0);
-}
-		
+        putchar('\n');
+        return (0);
+}	
